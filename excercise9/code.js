7d831cb74b224b7d2ada4b21caae245e783a8018
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-01-29 11:14:16 +0530

    excercise 9 fix

---
+++ excercise9/code.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const bl = require('bl');
+
+let counter = 0;
+const list = [];
+function printlist() {
+  list.forEach(val => console.log(val));
+}
+function getHttp(urlNo) {
+  http.get(process.argv[urlNo + 1], (response) => {
+    // console.log(process.argv[urlNo + 1]);
+    response.pipe(bl((error, data) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      list[urlNo - 1] = data.toString();
+      counter += 1;
+      if (counter === 3) {
+        printlist();
+      }
+    }));
+  });
+}
+getHttp(1);
+getHttp(3);
+getHttp(2);
+module.exports.printlist = printlist;
